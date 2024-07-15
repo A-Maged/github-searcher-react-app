3da67c3a78f9bcc396cce6b1704e6db4966a2272
a-maged <maged.dev@gmail.com>
2024-07-15 20:43:47 +0300

    refactor

--- client/src/app/authenticated-app.tsx
+++ client/src/app/authenticated-app.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage } from "../pages/home/home.page";
+import { HomePage } from "../pages/home";
 
 export function AuthenticatedApp() {
   return (
