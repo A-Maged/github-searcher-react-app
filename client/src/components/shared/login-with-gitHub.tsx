45152aa97bd7cef469109f1a89d7de0481d21735
a-maged <maged.dev@gmail.com>
2024-07-16 06:55:21 +0300

    add redirect_uri

--- client/src/components/shared/login-with-gitHub.tsx
+++ client/src/components/shared/login-with-gitHub.tsx
@@ -6,7 +6,7 @@ export function LoginWithGitHub() {
       className="btn-dark"
       href={`https://github.com/login/oauth/authorize?scope=user&client_id=${
         import.meta.env.VITE_GITHUB_CLIENT_ID
-      }`}
+      }&redirect_uri=${import.meta.env.VITE_GITHUB_REDIRECT_URI}`}
     >
       <FaGithub fontSize={30} />
       <span>Login with GitHub</span>
